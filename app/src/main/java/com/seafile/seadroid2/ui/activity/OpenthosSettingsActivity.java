3ecb6c8578bcbb98984bb8ad427d5a9be84c94eb
dongpeng <dongpeng@openthos.org>
2019-05-07 14:57:57 +0800

    Optimize layout for openthos setings
    Signed-off-by: dongpeng <dongpeng@openthos.org>

--- app/src/main/java/com/seafile/seadroid2/ui/activity/OpenthosSettingsActivity.java
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/OpenthosSettingsActivity.java
@@ -45,8 +45,8 @@ public class OpenthosSettingsActivity extends BaseActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        if (frameLayout !=null) frameLayout.removeAllViews();
-        switch (v.getId()){
+        if (frameLayout != null) frameLayout.removeAllViews();
+        switch (v.getId()) {
             case R.id.account_settings_view:
                 frameLayout.addView(accountSettings);
                 break;
@@ -56,18 +56,17 @@ public class OpenthosSettingsActivity extends BaseActivity implements View.OnCli
         }
     }
 
-
     @Override
     public boolean onHover(View v, MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_HOVER_ENTER:
-                accountButton.setBackgroundResource(R.drawable.recycle_item_backgroud);
-                accountButton.setTextColor(this
+                v.setBackgroundResource(R.drawable.recycle_item_backgroud);
+                ((TextView) v).setTextColor(this
                         .getResources().getColor(R.color.fancy_purple));
                 break;
             case MotionEvent.ACTION_HOVER_EXIT:
-                generalButton.setBackgroundResource(0);
-                generalButton.setTextColor(this
+                v.setBackgroundResource(0);
+                ((TextView) v).setTextColor(this
                         .getResources().getColor(R.color.fancy_left_gray));
                 break;
         }
